8ba74ca1d3713cb74316cd66e78cf055ed524d19
Nina Angelo <ninacelie@gmail.com>
2020-01-14 23:13:34 -0600

    added gems

--- lib/daily_horoscope/api.rb
+++ lib/daily_horoscope/api.rb
@@ -1,6 +1,7 @@
 class DailyHoroscope::API
     
-    BASE_URL = https://json.astrologyapi.com/v1/sun_sign_prediction/daily/:zodiacName
+    # BASE_URL = "https://json.astrologyapi.com/v1/sun_sign_prediction/daily/:zodiacName" 
 
 
 end 
+
